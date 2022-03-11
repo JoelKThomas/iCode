ed3955dcd04a4f144c577a11900a8df877606173
JoelKThomas <joelkthomas11@gmail.com>
2022-03-11 14:05:24 +0530

    Updated Coding questions

---
+++ Java/src/com/algoexpert/array/nonconstructiblechange/NonConstructibleChange.java
@@ -0,0 +1,28 @@
+
+package com.algoexpert.array.nonconstructiblechange;
+
+import java.util.Arrays;
+
+public class NonConstructibleChange {
+
+    public static int nonConstructibleChange(int[] coins) {
+        int maxChange = 0;
+        Arrays.sort(coins);
+        for (int i = 0; i < coins.length; i++) {
+            if (coins[i] <= maxChange + 1) {
+                maxChange += coins[i];
+            } else {
+                return maxChange + 1;
+            }
+        }
+        return maxChange+1;
+    }
+
+
+    public static void main(String[] args) {
+        int[] input = new int[]{5, 7, 1, 1, 2, 3, 22};
+        var actual = nonConstructibleChange(input);
+
+        System.out.println("Change: " + actual);
+    }
+}
