69f6ff773cc1ed0758c5d5dd59c7942b7903e2ca
Joel K Thomas <joelkthomas11@gmail.com>
2024-09-27 14:44:36 +0530

    Leetcode questions - 2390. Removing Stars From a String

--- Java/src/com/leetcode/RemovingStarsFromaString.java
+++ Java/src/com/leetcode/RemovingStarsFromaString.java
@@ -1,5 +1,37 @@
 package com.leetcode;
 
+import java.util.Stack;
+
 public class RemovingStarsFromaString {
+
+    public String removeStars(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(char c : s.toCharArray()){
+            if(c == '*'){
+                if(!stack.isEmpty()){
+                    stack.pop();
+                }
+            } else {
+                stack.push(c);
+            }
+        }
+
+        StringBuilder s1 = new StringBuilder();
+        for(int i = 0; i< stack.size(); i++){
+            s1.append(stack.get(i));
+        }
+        return s1.toString();
+
+    }
+    public static void main(String[] args) {
+        RemovingStarsFromaString removingStarsFromaString = new RemovingStarsFromaString();
+        String input1 = "leet**cod*e";
+        System.out.println(input1 + "--->" +removingStarsFromaString.removeStars(input1));
+
+
+        String input2 = "erase*****";
+        System.out.println(input2 + "--->" +removingStarsFromaString.removeStars(input2));
+
+    }
 
 }
