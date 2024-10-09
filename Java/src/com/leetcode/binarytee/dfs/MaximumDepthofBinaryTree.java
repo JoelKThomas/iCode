e0e6ee2b61229d551ac8599bb335d4d8fe37cbef
Joel K Thomas <joelkthomas11@gmail.com>
2024-10-09 20:39:25 +0530

    Leet Code DFS

--- Java/src/com/leetcode/binarytee/dfs/MaximumDepthofBinaryTree.java
+++ Java/src/com/leetcode/binarytee/dfs/MaximumDepthofBinaryTree.java
@@ -2,11 +2,32 @@ package com.leetcode.binarytee.dfs;
 
 public class MaximumDepthofBinaryTree {
 
+    public static int maxDepth(TreeNode root) {
+      return dfs(root, 0);
+    }
+
+    public static int dfs(TreeNode root, int maxLength) {
+        if(root == null){
+            return maxLength;
+        }
+
+        return Math.max(dfs(root.left, maxLength), dfs(root.right, maxLength)) + 1;
+    }
+
+
 
 
     public static void main(String[] args) {
-        TreeNode root = new TreeNode(3, new TreeNode(9,null, null),
-                new TreeNode(20, new TreeNode(15, null, null),  new TreeNode(7, null, null));
+
+        TreeNode root = new TreeNode(3,
+                new TreeNode(9,null, null),
+                new TreeNode(20,
+                        new TreeNode(15, null, null),
+                        new TreeNode(7, null, null)));
+
+        root.printPreOrder();
+
+        System.out.println("\nMaximum Depth : " +  maxDepth(root));
     }
 
 }
--- Java/src/com/leetcode/binarytee/dfs/TreeNode.java
+++ Java/src/com/leetcode/binarytee/dfs/TreeNode.java
@@ -15,7 +15,18 @@ public class TreeNode {
 
     TreeNode(int val, TreeNode left, TreeNode right) {
         this.val = val;
-        this.left = right;
+        this.left = left;
         this.right = right;
     }
+
+    public void printPreOrder() {
+        System.out.print(this.val + " ");  // Print current node (this)
+        if (this.left != null) {
+            this.left.printPreOrder();     // Recur on left child
+        }
+        if (this.right != null) {
+            this.right.printPreOrder();    // Recur on right child
+        }
+    }
+
 }
